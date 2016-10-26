b8b8449f6bc80885c9692e859b77f72d78e20332
Vascos <dellostevasco@yahoo.it>
2016-10-27 00:31:16 +0200

    somma

---
+++ ProgramSomma.js
@@ -0,0 +1,33 @@
+'use sctrict'
+
+	function Somma1(a, b){
+		var a=3;
+		var b=3:
+			console.log(a + b);			
+	}
+
+	const Somma2 = function(c, d){
+
+			var c =3;
+			var d =3;
+			console.log(c + d);
+	}
+
+	}
+
+	var function Somma3 = (e, f) => {
+			var e = 3;
+			var f = 3;
+			console.log(e + f);
+	}
+
+
+
+	g = 10;
+
+	const IfFunction = function (){
+		if (g > 9) {
+			console.log('grandissimo');
+			var g;
+		}
+	}
